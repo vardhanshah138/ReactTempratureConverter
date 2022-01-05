5f57d0574881ae8ff5e52083a3b4ffacc50b374f
vardhanshah138 <vardhan.shah@pirimidtech.com>
2022-01-05 17:54:55 +0530

    Added Temprature input and main class

--- script.js
+++ script.js
@@ -2,7 +2,38 @@ function BoilingPoint(props){
     return (<p>The Water would {(props.celsius !='' && props.celsius >=100) ? "" : "not"} boil</p>);
 }
 
+class TempratureInput extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const temprature = this.props.temprature;
+        const scale = this.props.scale;
+        return(
+            <div>
+                <h5> Enter Temprature in {scale} </h5>
+                <input value={temprature}/>
+            </div>
+        )
+    }
+}
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <TempratureInput temprature = {100} scale ='farehenite' />
+                <TempratureInput temprature = {100} scale ='celsius' />
+                <BoilingPoint celsius = {100}/>
+            </div>
+        )
+    }
+
+}
+
+
 ReactDOM.render(
-    <BoilingPoint celsius = {100}/>,
-    document.getElementById('root')
+    <Main />
+    ,document.getElementById('root')
 );
